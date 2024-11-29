53029ef38955f9ee7d7957fb97a97b85982877ba
kienbuiii <kien36652@gmail.com>
2024-11-30 00:25:09 +0700

    a

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Thêm meta tag để tránh cache
+const meta = document.createElement('meta');
+meta.httpEquiv = 'Cache-Control';
+meta.content = 'no-cache, no-store, must-revalidate';
+document.head.appendChild(meta);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +17,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
